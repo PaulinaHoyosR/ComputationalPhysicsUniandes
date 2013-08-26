90a4efbfd7b06c6ae502729684935e2b9f32138a
PaulinaHoyosR <paulinahoyosrestrepo@gmail.com>
2013-08-26 02:36:33 -0500

    modified code for wc

--- homework/hw3/PaulinaHoyos_wc.py
+++ homework/hw3/PaulinaHoyos_wc.py
@@ -1,9 +1,8 @@
 import os
-filenames = os.listdir('./')
+filenames = os.listdir(os.getcwd())
 
 for filename in filenames:
     file = open(filename, 'r')
     file_lines = file.readlines()
     print filename, len(file_lines)
     file.close()
-    
